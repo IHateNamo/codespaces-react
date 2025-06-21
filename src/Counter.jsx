a6c7aa6a3d74d478441045a1daeee3a725052752
IHateNamo <thamatijaru@gmail.com>
2025-06-21 08:52:09 +0000

    React day 1.2

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,29 +1,22 @@
 import { useState } from 'react';
 import './Counter.css';
-export default function Counter(){
-    const [count,setCount]=useState(0)
-    return(<>
-    <div>
-        <center>
-         <button onClick={() => setCount(count - 1)}>
-            Click me for -
-        </button>
-        </center>
-    <center>
-        <button onClick={() => setCount(count + 1)}>
-            Click me for + 
-        </button>
-    </center>
-    <center>
-         <button onClick={() => setCount(0)}>
-            Click me to clear
-        </button>
-        </center>
-        
-    </div>
-        <center>
-        <p>You clicked {count} time
-        </p>
-        </center>
-    </>);
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    return (
+        <div className="counter-container">
+            <div className="button-row">
+                <button onClick={() => setCount(count - 1)}>
+                    Click me for -
+                </button>
+                <button onClick={() => setCount(count + 1)}>
+                    Click me for +
+                </button>
+                <button onClick={() => setCount(0)}>
+                    Click me to clear
+                </button>
+            </div>
+            <p className="counter-text">You clicked {count} time</p>
+        </div>
+    );
 }
